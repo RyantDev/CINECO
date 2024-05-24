5be398bbbe36dc689c73713cf96c7f58f4f747f3
RyantDev <167722988+RyantDev@users.noreply.github.com>
2024-05-23 21:50:53 -0500

    CARDS

--- src/app/components/shared/catalogo/catalogo.component.ts
+++ src/app/components/shared/catalogo/catalogo.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-catalogo',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './catalogo.component.html',
   styleUrl: './catalogo.component.css'
 })
 export class CatalogoComponent {
 
-}
+title : string ='pelicula.title';
+peliculas  = [
+      { title: 'silent', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'memoi', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'titulopeli', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'DAR', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'BL', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'MARIO', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'HOL', imageUrl: 'assets/img/kimetsu.jpg'},
+      { title: 'HAL', imageUrl: 'assets/img/kimetsu.jpg'},
+];
+      }
